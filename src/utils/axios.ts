35e728688343280a86a058e2ce4df6acef70005a
1216301160 <1216301160@qq.com>
2026-05-29 07:32:48 +0800

    fix: axios baseURL使用环境变量，生产环境直接请求后端地址

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import { message } from 'antd';
 
 // 创建 axios 实例
 const service: AxiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_DATA_API_BASE_URL || '/api',
   timeout: 600000,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
